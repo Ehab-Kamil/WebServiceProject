3a2eeb0c2334937407fbf75d7a71df1a8e5e0346
yoka132 <aya.farag3011@gmail.com>
2016-06-10 12:24:45 +0200

    vehicle web service (insert and get are working now)

--- src/main/java/webServices/Vehicles.java
+++ src/main/java/webServices/Vehicles.java
@@ -7,7 +7,9 @@ package webServices;
 
 import webServicesHandlers.Handler;
 import com.google.gson.Gson;
+import java.util.ArrayList;
 import java.util.List;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -27,17 +29,36 @@ import pojo.Year;
 @Path("/vehicle")
 public class Vehicles {
 
-    Handler handler = new Handler();
-    Gson gson = new Gson();
+    Handler handler;
+    Gson gson;
+
+    public Vehicles() {
+        handler = new Handler();
+        gson = new Gson();
+
+    }
 
     @GET
     @Path("/makes")
     @Produces("application/json")
     public String getMakes() {
         List<Make> lst = handler.getMake();
-        String x = gson.toJson(lst);
-        return x;
-
+        if (lst.size() > 0) {
+            List<Make> mkresult = new ArrayList<>();
+            for (int i = 0; i < lst.size(); i++) {
+                Make mk = lst.get(i);
+                mk.setModels(null);
+                // mk.setServiceProviders(null);
+                mkresult.add(mk);
+            }
+            String x = gson.toJson(mkresult);
+            return x;
+        } else {
+            JSONObject obj = new JSONObject();
+            obj.append("msg", "400");
+            obj.append("msg", "no supported trims");
+            return obj.toString();
+        }
     }
 
     @GET
@@ -45,8 +66,23 @@ public class Vehicles {
     @Produces("application/json")
     public String getModels(@QueryParam("make") String make) {
         List<Model> lst = handler.getModelByMake(make);
-        String x = gson.toJson(lst);
-        return x;
+        if (lst.size() > 0) {
+            List<Model> mkresult = new ArrayList<>();
+            for (int i = 0; i < lst.size(); i++) {
+                Model model = lst.get(i);
+                model.setMake(null);
+                model.setVehicleModels(null);
+                mkresult.add(model);
+            }
+            String x = gson.toJson(mkresult);
+            //String x = gson.toJson(lst);
+            return x;
+        } else {
+            JSONObject obj = new JSONObject();
+            obj.append("msg", "400");
+            obj.append("msg", "no supported models");
+            return obj.toString();
+        }
     }
 
     @GET
@@ -54,8 +90,21 @@ public class Vehicles {
     @Produces("application/json")
     public String getTrim(@QueryParam("model") String model, @QueryParam("year") String year) {
         List<Trim> lst = handler.getTrim(model, year);
-        String x = gson.toJson(lst);
-        return x;
+        if (lst.size() > 0) {
+            List<Trim> trimresult = new ArrayList<>();
+            for (int i = 0; i < lst.size(); i++) {
+                Trim trim = lst.get(i);
+                trim.setVehicleModels(null);
+                trimresult.add(trim);
+            }
+            String x = gson.toJson(trimresult);
+            return x;
+        } else {
+            JSONObject obj = new JSONObject();
+            obj.append("msg", "400");
+            obj.append("msg", "no supported trims");
+            return obj.toString();
+        }
     }
 
     @GET
@@ -63,15 +112,30 @@ public class Vehicles {
     @Produces("application/json")
     public String getYear(@QueryParam("model") String model) {
         List<Year> lst = handler.getYear(model);
-        return null;
+        if (lst.size() > 0) {
+            List<Year> yearResult = new ArrayList<>();
+            for (int i = 0; i < lst.size(); i++) {
+                Year year = lst.get(i);
+                year.setVehicleModels(null);
+                yearResult.add(year);
+            }
+            String x = gson.toJson(yearResult);
+
+            return x;
+        } else {
+            JSONObject obj = new JSONObject();
+            obj.append("msg", "400");
+            obj.append("msg", "no supported years");
+            return obj.toString();
+        }
     }
 
     @POST
-    //  @Path("/{make}/{model}/{year}/{trim}")
     @Produces("application/json")
 
-    public String addVehicle(@QueryParam("make") String make, @QueryParam("model") String model, @QueryParam("year") String year, @QueryParam("trim") String trim) {
-        boolean lst = handler.addVehicle(make, model, year, trim);
+    public String addVehicle(@FormParam("model") String model, @FormParam("year") String year,
+            @FormParam("trim") String trim, @FormParam("userId") int uId, @FormParam("carName") String carName, @FormParam("intialOdemeter") int intialOdemeter) {
+        boolean lst = handler.addVehicle(model, year, trim, uId, carName, intialOdemeter);
         if (lst) {
             JSONObject obj = new JSONObject();
             obj.append("msg", "200");
