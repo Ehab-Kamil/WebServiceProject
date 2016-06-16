68e67de17b28791069a03d8739b58d467f2a7747
Ehab-Kamil <Ehabkamil2@gmail.com>
2016-06-16 09:49:45 +0200

    TripService Done

--- src/main/java/webServices/TripService.java
+++ src/main/java/webServices/TripService.java
@@ -18,33 +18,50 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.xml.bind.DatatypeConverter;
 import org.json.JSONObject;
+import pojo.Coordinates;
+import pojo.Trips;
 import webServicesHandlers.Handler;
 
 /**
  *
  * @author yoka
  */
-@Path("trip")
+@Path("/tripService")
 public class TripService {
-      Handler handler=new Handler();
+
+    final String errorMessage = "error";
+    final String successMessage = "success";
+
+    Handler handler;
+
+    public TripService() {
+        handler = new Handler();
+    }
+    @Path("/addTrip")
     @GET
     @Produces("application/json")
     public String addTrip(@QueryParam("vehicleId") int vehicleId,
-            @QueryParam("initialOdemeter") int initialOdemeter,@QueryParam("coveredMilage")int coveredMilage
-    
-    )
-    {
-    return null;
+            @QueryParam("initialOdemeter") int initialOdemeter, @QueryParam("coveredMilage") int coveredMilage) {
+        Trips trips = handler.addTrip(initialOdemeter, coveredMilage, vehicleId);
+        JSONObject obj = new JSONObject();
+        JSONObject obj1 = new JSONObject(trips);
+        obj.append("result", obj1);
+        obj.append("status", successMessage);
+        return obj.toString();
     }
-    
+    @Path("/addCoordinates")
     @GET
     @Produces("application/json")
-    @Path("/coordinate")
-   public String addCoordinates(@QueryParam("vehicleId") int vehicleId,
-            @QueryParam("longitude") float longitude,@QueryParam("latitude") float latitude)
-   {
-   return null;
-   }
+    public String addCoordinates(@QueryParam("vehicleId") int vehicleId,
+            @QueryParam("longitude") float longitude, @QueryParam("latitude") float latitude, @QueryParam("tripId") int tripId) {
+        Coordinates coordinates = handler.addCoordinatesToTrip(longitude, latitude, tripId);
+        JSONObject obj = new JSONObject();
+        JSONObject obj1 = new JSONObject(coordinates);
+        obj.append("result", obj1);
+        obj.append("status", successMessage);
+        return obj.toString();
+    }
+
     @POST
     public String upload(@FormParam("image") String img, @FormParam("userId") int userId, @FormParam("fileExt") String fileExt) {
         byte[] arr = DatatypeConverter.parseBase64Binary(img);
@@ -59,7 +76,7 @@ public class TripService {
         return obj.toString();
     }
 
-    public void writeToFile(byte[] array, String ext, int userId) throws IOException {
+    private void writeToFile(byte[] array, String ext, int userId) throws IOException {
         String image = "/Users/yoka/desktop/Uploaded/pic" + userId + "." + ext;
         try (FileOutputStream fileOuputStream = new FileOutputStream(image)) {
             fileOuputStream.write(array);
